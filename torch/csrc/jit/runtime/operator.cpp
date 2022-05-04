bcbf6235aa2bff3fc2bac9b897724d46c5f1e215
Elias Ellison <eellison@devfair044.h1.fair>
2022-05-04 16:49:01 -0700

    [JIT] Add Example Test showing how to add custom post pass
    ghstack-source-id: a95a2c3e54d27270a50d446abcd02e728dbdfb4f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76860

--- torch/csrc/jit/runtime/operator.cpp
+++ torch/csrc/jit/runtime/operator.cpp
@@ -6,6 +6,7 @@
 #include <c10/util/irange.h>
 #include <torch/csrc/jit/frontend/edit_distance.h>
 
+#include <jit/ir/ir.h>
 #include <queue>
 #include <utility>
 #include <vector>
@@ -268,7 +269,7 @@ bool printerHasSpecialCaseFor(Symbol sym) {
   };
 
   return handled.count(sym) || unneeded.count(sym) ||
-      !required_namespaces.count(sym.ns());
+      !required_namespaces.count(sym.ns()) || IsFusionSubgraphKind(sym);
 }
 
 } // anonymous namespace
@@ -350,7 +351,7 @@ bool aliasAnalysisHasSpecialCaseFor(Symbol symbol) {
 void registerOperator(Operator&& op) {
   if (op.schema().is_varret()) {
     Symbol s = Symbol::fromQualString(op.schema().name());
-    if (!printerHasSpecialCaseFor(s)) {
+    if (!printerHasSpecialCaseFor(s) && !IsFusionSubgraphKind(s)) {
       AT_ERROR(
           "Missing special case in python printer for non-schematized"
           " operator ",
