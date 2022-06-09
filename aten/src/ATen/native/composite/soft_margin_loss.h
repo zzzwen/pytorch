dfa54669ee53fa17958bb301aa08b37f8a0ab2c2
Yukio Siraichi <yukio.siraichi@gmail.com>
2022-06-09 20:58:13 +0900

    Implement `soft_margin_loss` as dispatch-less composite kernel.
    This PR makes `soft_margin_loss` kernel into a dispatch-less composite kernel.
    Here are the main changes:
    
    - `Loss.cpp`
        - Removed `soft_margin_loss` function definitions
    - `composite/soft_margin_loss.h`
        - Add the templated functions
    - `native_functions.yaml`
        - Add the `composite` key to `soft_margin_loss`
    - `generated_ops.cpp`
        - Replaced `at::native::soft_margin_loss` with `at::cpu::soft_margin_loss`
    
    ghstack-source-id: 1a6595085147106515f3a525a9c55d9e0e2421a2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77486

---
+++ aten/src/ATen/native/composite/soft_margin_loss.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <ATen/core/Reduction.h>
+#include <ATen/core/TensorBody.h>
+
+namespace at {
+namespace native {
+
+template <typename OPS>
+inline Tensor apply_reduction(const Tensor& output, int64_t reduction) {
+  switch (reduction) {
+    case at::Reduction::Mean:
+      return OPS::mean(output);
+    case at::Reduction::Sum:
+      return OPS::sum(output);
+    case at::Reduction::None:
+      return output;
+    default:
+      TORCH_INTERNAL_ASSERT(false, "Invalid reduction: ", (int64_t)reduction);
+  }
+}
+
+template <typename OPS>
+Tensor& soft_margin_loss_out(
+    const Tensor& input,
+    const Tensor& target,
+    int64_t reduction,
+    Tensor& output) {
+  // compute inplace variant of: output = at::log(1. + at::exp(-input *
+  // target));
+  OPS::neg_out(output, input);
+  OPS::mul_(output, target);
+  OPS::exp_(output);
+  OPS::add_(output, 1.);
+  OPS::log_(output);
+
+  auto tmp = apply_reduction<OPS>(output, reduction);
+  if (!tmp.is_same(output)) {
+    OPS::resize_(output, {});
+    OPS::copy_(output, tmp);
+  }
+
+  return output;
+}
+
+template <typename OPS>
+Tensor soft_margin_loss(
+    const Tensor& input,
+    const Tensor& target,
+    int64_t reduction) {
+  auto output = OPS::empty({0}, input.options());
+  OPS::soft_margin_loss_out(output, input, target, reduction);
+  return output;
+}
+
+} // namespace native
+} // namespace at
