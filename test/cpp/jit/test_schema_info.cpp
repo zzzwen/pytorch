c0a151490dcc83ee6d1c2f40fbeae9ea8b8d0a6e
goldenxuett <ttanx@fb.com>
2022-06-28 15:26:25 -0700

    [JIT] Add determinism checks for ops in SchemaInfo class
    ghstack-source-id: ab430d90be80e6fb195f2645cca0badf1e836486
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80186

--- test/cpp/jit/test_schema_info.cpp
+++ test/cpp/jit/test_schema_info.cpp
@@ -1,18 +1,13 @@
 #include <gtest/gtest.h>
-#include <torch/csrc/jit/runtime/operator.h>
 #include <torch/csrc/utils/schema_info.h>
 
 namespace torch {
 namespace utils {
 namespace {
-const c10::FunctionSchema getSchema(const char* name) {
-  return torch::jit::getOperatorForLiteral(name)->schema();
-}
 
 TEST(SchemaInfoIsMutableTest, Basic) {
-  auto schema = getSchema(
+  SchemaInfo schema_info(
       "aten::sub_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha=1) -> (Tensor(a!))");
-  SchemaInfo schema_info(schema);
   ASSERT_TRUE(schema_info.isMutating(0));
   ASSERT_TRUE(schema_info.isMutating("self"));
   ASSERT_FALSE(schema_info.isMutating(1));
@@ -22,19 +17,16 @@ TEST(SchemaInfoIsMutableTest, Basic) {
 }
 
 TEST(SchemaInfoIsMutableTest, InvalidArgument) {
-  auto schema = getSchema(
+  SchemaInfo schema_info(
       "aten::sub_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha=1) -> (Tensor(a!))");
-  SchemaInfo schema_info(schema);
   ASSERT_THROW(schema_info.isMutating(-1), c10::Error);
   ASSERT_THROW(schema_info.isMutating(4), c10::Error);
   ASSERT_THROW(schema_info.isMutating("named_argument"), c10::Error);
 }
 
 TEST(SchemaInfoAreAliasingTest, Basic) {
-  auto schema = getSchema(
+  SchemaInfo schema_info(
       "aten::sub_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha=1) -> (Tensor(a!))");
-  SchemaInfo schema_info(schema);
-
   ASSERT_TRUE(schema_info.areAliasing(
       {SchemaArgType::input, 0}, {SchemaArgType::output, 0}));
   ASSERT_FALSE(schema_info.areAliasing(
@@ -44,9 +36,8 @@ TEST(SchemaInfoAreAliasingTest, Basic) {
 }
 
 TEST(SchemaInfoAreAliasingTest, InvalidArgument) {
-  auto schema = getSchema(
+  SchemaInfo schema_info(
       "aten::sub_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha=1) -> (Tensor(a!))");
-  SchemaInfo schema_info(schema);
   ASSERT_THROW(
       schema_info.areAliasing(
           {SchemaArgType::input, -1}, {SchemaArgType::output, 0}),
@@ -58,11 +49,19 @@ TEST(SchemaInfoAreAliasingTest, InvalidArgument) {
 }
 
 TEST(SchemaInfoAreAliasingTest, Wildcard) {
-  auto schema = getSchema(
+  SchemaInfo schema_info(
       "aten::split.Tensor(Tensor(a -> *) self, int split_size, int dim=0) -> Tensor(a)[]");
-  SchemaInfo schema_info(schema);
   ASSERT_TRUE(schema_info.areAliasing(
       {SchemaArgType::input, 0}, {SchemaArgType::output, 0}));
+}
+
+TEST(SchemaInfoIsDeterministicTest, Basic) {
+  SchemaInfo deterministic_schema_info(
+      "aten::sub_.Tensor(Tensor(a!) self, Tensor other, *, Scalar alpha=1) -> (Tensor(a!))");
+  SchemaInfo nondeterministic_schema_info(
+      "aten::dropout(Tensor input, float p, bool train) -> Tensor");
+  ASSERT_TRUE(deterministic_schema_info.isDeterministic());
+  ASSERT_FALSE(nondeterministic_schema_info.isDeterministic());
 }
 } // namespace
 } // namespace utils
