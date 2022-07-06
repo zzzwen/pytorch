56bdb5b9a70fe9d56aff6019f68fe37c28fedeb5
lezcano <lezcano-93@hotmail.com>
2022-07-06 11:57:32 +0000

    Add shortcuts for refs.pow
    As per title
    
    ghstack-source-id: dfffde1c3ef6a55ca55b45793033fa91ec43f8ad
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80219

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -20700,8 +20700,13 @@ python_ref_db = [
         "_refs.pow",
         torch_opinfo_name="pow",
         skips=(
+            # For the complex32:
             # Reference result was farther (inf) from the precise
             # computation than the torch result was (nan)!
+            #
+            # For the int32:
+            # RuntimeError: entry_it != disjointSetMap().end() INTERNAL ASSERT FAILED
+            # at "/var/lib/jenkins/workspace/torch/csrc/jit/codegen/cuda/disjoint_set.h":218
             DecorateInfo(
                 unittest.expectedFailure, 'TestCommon', 'test_python_ref_executor',
                 dtypes=(torch.complex32,),
