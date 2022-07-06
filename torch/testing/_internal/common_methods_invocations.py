c163ca9c1ac472bf9267195cb3fa87ecf73083b9
lezcano <lezcano-93@hotmail.com>
2022-07-06 11:57:33 +0000

    Add prim.svd, refs.linalg.svd, and refs.linalg.svdvals
    This is the first prim / ref added that has multiple returns.
    There is an issue with `out_wrapper_multi` as currently implemented
    (left a note). It assumes that the API is `svd(X, U=U, S=S, Vh=Vh)`,
    when it's actually `svd(X, out=(U, S, Vh))`.
    
    Even more, if we want to model PyTorch exactly, it should return a
    `torch.return_types.svd`, rather than a `Tuple`.
    
    There is an issue with
    As per title
    
    ghstack-source-id: 5f7d690a7aad2004096d4f7dee8096454ca32c0a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78616

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -7165,7 +7165,7 @@ def sample_inputs_svd(op_info, device, dtype, requires_grad=False, **kwargs):
     make_fullrank = make_fullrank_matrices_with_distinct_singular_values
     make_arg = partial(make_fullrank, dtype=dtype, device=device, requires_grad=requires_grad)
 
-    is_linalg_svd = (op_info.name == "linalg.svd")
+    is_linalg_svd = ("linalg.svd" in op_info.name)
     batches = [(), (0, ), (3, )]
     ns = [0, 3, 5]
 
@@ -21114,6 +21114,16 @@ python_ref_db = [
     ReductionPythonRefInfo(
         "_refs.linalg.vector_norm",
         torch_opinfo_name="linalg.vector_norm",
+        supports_out=True,
+    ),
+    PythonRefInfo(
+        "_refs.linalg.svd",
+        torch_opinfo_name="linalg.svd",
+        supports_out=True,
+    ),
+    PythonRefInfo(
+        "_refs.linalg.svdvals",
+        torch_opinfo_name="linalg.svdvals",
         supports_out=True,
     ),
     #
